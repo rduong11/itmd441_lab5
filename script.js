1911ee86a9db4f5535a83376bed7f07de63450be
rduong11 <raymondduong12@gmail.com>
2025-04-16 11:11:57 -0500

    rough implementation. total bill is still bugging out

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const billAmount = document.getElementById("bill-total");
 const billWithTaxField = document.getElementById("bill-w-tax");
 const errorMessage = document.getElementById("error-msg");
+const billWithTaxAndTipField = document.getElementById("bill-w-tip-tax");
+
+const tipSlider = document.getElementById("tip-slider");
+const tipPercent = document.getElementById("tip-percent");
+const tipAmountField = document.getElementById("tip-amount");
+
 
 billAmount.addEventListener("input", (e) =>
 {
@@ -10,6 +16,7 @@ billAmount.addEventListener("input", (e) =>
     {
         const billWithTax = value * 1.11;
         billWithTaxField.value = billWithTax.toFixed(2);
+        billWithTaxAndTipField.value = billWithTax.toFixed(2);
     }
     else
     {
@@ -18,3 +25,16 @@ billAmount.addEventListener("input", (e) =>
     }
 });
 
+tipSlider.addEventListener("change", (e) =>
+{
+    userTip = e.target.value;
+    tipPercent.textContent = userTip;
+    const tipAmount = parseFloat(((userTip / 100) * billAmount.value)).toFixed(2);
+    tipAmountField.value = tipAmount;
+    console.log(billWithTaxField.value);
+    console.log(tipAmount);
+    console.log(billWithTaxField.value + tipAmountField.value);
+    const totalBill = billWithTaxField.value + (tipAmount * 100);
+    billWithTaxAndTipField.value = parseFloat(totalBill).toFixed(2);
+
+});
